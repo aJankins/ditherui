a207d77bc4124afd7121a47a0cdb1ece105ad1bc
ajfyb <mctech26@gmail.com>
2023-07-29 20:16:00 +0200

    Updated floydsteinberg to be better

--- src/dither/floydsteinberg.rs
+++ src/dither/floydsteinberg.rs
@@ -2,31 +2,31 @@ use image::{DynamicImage, Pixel};
 
 use crate::utils::u8ops::average;
 
-pub fn floyd_steinberg_mono_dither(image: DynamicImage) -> DynamicImage {
-    let collapser = |num: i64| if num < 128 { 0 } else { 255 };
+use super::pixel::{MonoPixel, TWO_BIT};
 
+pub fn floyd_steinberg_mono_dither(image: DynamicImage) -> DynamicImage {
     let mut rgb8_image = image.into_rgb8();
     let (xdim, ydim) = rgb8_image.dimensions();
-    let mut error_matrix = vec![vec![0 as i64; (xdim+1) as usize]; (ydim+1) as usize];
+    let mut error_matrix = vec![vec![0 as i32; (xdim+1) as usize]; (ydim+1) as usize];
 
 
     for (x, y, pixel) in rgb8_image.enumerate_pixels_mut() {
         let xs = x as usize;
         let ys = y as usize;
 
-        let mono = average(pixel.channels()) as i64 + *(&error_matrix[ys][xs]);
-        let threshold = collapser(mono);
+        let mono = MonoPixel::mono_from(pixel).add_error(error_matrix[ys][xs]);
+        let quantized = mono.quantize(TWO_BIT);
 
-        let error = mono - threshold;
+        let error = mono.get_error(&quantized);
 
-        error_matrix[ys][xs+1] = &error_matrix[ys][xs+1] + ((error * 7) / 16) as i64;
-        if xs > 0 { error_matrix[ys+1][xs-1] = &error_matrix[ys+1][xs-1] + ((error * 5) / 16) as i64; }
-        error_matrix[ys+1][xs] = &error_matrix[ys+1][xs] + ((error * 3) / 16) as i64;
-        error_matrix[ys+1][xs+1] = &error_matrix[ys+1][xs+1] + ((error * 1) / 16) as i64;
+        error_matrix[ys][xs+1] = &error_matrix[ys][xs+1] + ((error * 7) / 16) as i32;
+        if xs > 0 { error_matrix[ys+1][xs-1] = &error_matrix[ys+1][xs-1] + ((error * 5) / 16) as i32; }
+        error_matrix[ys+1][xs] = &error_matrix[ys+1][xs] + ((error * 3) / 16) as i32;
+        error_matrix[ys+1][xs+1] = &error_matrix[ys+1][xs+1] + ((error * 1) / 16) as i32;
 
-        pixel[0] = threshold as u8;
-        pixel[1] = threshold as u8;
-        pixel[2] = threshold as u8;
+        pixel[0] = quantized.get();
+        pixel[1] = quantized.get();
+        pixel[2] = quantized.get();
     }
 
     DynamicImage::ImageRgb8(rgb8_image)
