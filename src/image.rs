4470782f903bdc6f75021f62200467e7b8cc5f9f
ajfyb <mctech26@gmail.com>
2023-07-30 02:49:17 +0200

    Added conversion for img to base64

--- src/image.rs
+++ src/image.rs
@@ -1,3 +1,6 @@
+use std::io::Cursor;
+
+use base64::Engine;
 use image::{self, DynamicImage};
 use image::io::Reader as ImageReader;
 
@@ -5,4 +8,10 @@ pub fn load_image(path: &str) -> DynamicImage {
     let opened = ImageReader::open(path).expect(format!("Couldn't open image at '{}'", path).as_str());
     let decoded = opened.decode().expect("Failed to decode image.");
     return decoded;
+}
+
+pub fn image_to_b64(image: DynamicImage) -> String {
+    let mut buf = Vec::new();
+    image.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Png).unwrap();
+    base64::engine::general_purpose::STANDARD_NO_PAD.encode(buf)
 }
