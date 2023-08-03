bb3876df48c48f0a8bca3dcc351682bc58504b38
ajfyb <mctech26@gmail.com>
2023-08-03 16:23:31 +0200

    Cleaned up API

--- src/pixel/hsl.rs
+++ src/pixel/hsl.rs
@@ -12,74 +12,13 @@ impl From<(f32, f32, f32)> for HslPixel {
 
 impl From<RgbPixel> for HslPixel {
     fn from(value: RgbPixel) -> Self {
-        let (r, g, b) = value.get();
-        let (r, g, b) = (r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0);
-
-        let rgb_max = (r.max(g).max(b)) as f32;
-        let rgb_min = (r.min(g).min(b)) as f32;
-        let chroma = (rgb_max - rgb_min) as f32;
-
-        let hue = if chroma == 0.0 {
-            0.0
-        } else if rgb_max == r {
-            ((g - b) / chroma) % 6.0
-        } else if rgb_max == g {
-            ((b - r) / chroma) + 2.0
-        } else if rgb_max == b {
-            ((r - g) / chroma) + 4.0
-        } else {
-            panic!(
-                "None of R:{} G:{} B:{} matched the RGB_MAX:{}",
-                r, g, b, rgb_max
-            )
-        } * 60.0;
-
-        let lightness = (rgb_max + rgb_min) / 2.0;
-
-        let saturation = if lightness == 0.0 || lightness == 1.0 {
-            0.0
-        } else {
-            chroma / (1.0 - (2.0 * lightness - 1.0).abs())
-        };
-
-        return HslPixel(hue, saturation, lightness);
+        Self::from_rgb(&value)
     }
 }
 
 impl Into<RgbPixel> for HslPixel {
     fn into(self) -> RgbPixel {
-        let chroma = (1.0 - (2.0 * self.2 - 1.0).abs()) * self.1;
-        let hue_degree = self.get_normalized_hue() / 60.0;
-        let x = chroma * (1.0 - ((hue_degree % 2.0) - 1.0).abs());
-
-        let hue_degree = hue_degree as i8;
-
-        let (r1, g1, b1) = if hue_degree >= 0 && hue_degree < 1 {
-            (chroma, x, 0.0)
-        } else if hue_degree < 2 {
-            (x, chroma, 0.0)
-        } else if hue_degree < 3 {
-            (0.0, chroma, x)
-        } else if hue_degree < 4 {
-            (0.0, x, chroma)
-        } else if hue_degree < 5 {
-            (x, 0.0, chroma)
-        } else if hue_degree < 6 {
-            (chroma, 0.0, x)
-        } else {
-            panic!(
-                "Hue degree should be between 0 and 1 - was actually: {}",
-                hue_degree
-            )
-        };
-
-        let m = self.2 - (chroma / 2.0);
-
-        RgbPixel::new(
-            ((r1 + m) * 255.0).round() as u8,
-            ((g1 + m) * 255.0).round() as u8,
-            ((b1 + m) * 255.0).round() as u8,
-        )
+        self.as_rgb()
     }
 }
 
@@ -143,5 +82,74 @@ impl HslPixel {
     /// Retrieves the (h, s, l) values.
     pub fn get(&self) -> (f32, f32, f32) {
         (self.0, self.1, self.2)
+    }
+
+    pub fn from_rgb(rgb: &RgbPixel) -> HslPixel {
+        let (r, g, b) = rgb.get();
+        let (r, g, b) = (r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0);
+
+        let rgb_max = (r.max(g).max(b)) as f32;
+        let rgb_min = (r.min(g).min(b)) as f32;
+        let chroma = (rgb_max - rgb_min) as f32;
+
+        let hue = if chroma == 0.0 {
+            0.0
+        } else if rgb_max == r {
+            ((g - b) / chroma) % 6.0
+        } else if rgb_max == g {
+            ((b - r) / chroma) + 2.0
+        } else if rgb_max == b {
+            ((r - g) / chroma) + 4.0
+        } else {
+            panic!(
+                "None of R:{} G:{} B:{} matched the RGB_MAX:{}",
+                r, g, b, rgb_max
+            )
+        } * 60.0;
+
+        let lightness = (rgb_max + rgb_min) / 2.0;
+
+        let saturation = if lightness == 0.0 || lightness == 1.0 {
+            0.0
+        } else {
+            chroma / (1.0 - (2.0 * lightness - 1.0).abs())
+        };
+
+        HslPixel(hue, saturation, lightness)
+    }
+
+    pub fn as_rgb(&self) -> RgbPixel {
+        let chroma = (1.0 - (2.0 * self.2 - 1.0).abs()) * self.1;
+        let hue_degree = self.get_normalized_hue() / 60.0;
+        let x = chroma * (1.0 - ((hue_degree % 2.0) - 1.0).abs());
+
+        let hue_degree = hue_degree as i8;
+
+        let (r1, g1, b1) = if hue_degree >= 0 && hue_degree < 1 {
+            (chroma, x, 0.0)
+        } else if hue_degree < 2 {
+            (x, chroma, 0.0)
+        } else if hue_degree < 3 {
+            (0.0, chroma, x)
+        } else if hue_degree < 4 {
+            (0.0, x, chroma)
+        } else if hue_degree < 5 {
+            (x, 0.0, chroma)
+        } else if hue_degree < 6 {
+            (chroma, 0.0, x)
+        } else {
+            panic!(
+                "Hue degree should be between 0 and 1 - was actually: {}",
+                hue_degree
+            )
+        };
+
+        let m = self.2 - (chroma / 2.0);
+
+        RgbPixel::new(
+            ((r1 + m) * 255.0).round() as u8,
+            ((g1 + m) * 255.0).round() as u8,
+            ((b1 + m) * 255.0).round() as u8,
+        )
     }
 }
--- src/pixel/lab.rs
+++ src/pixel/lab.rs
@@ -1,7 +1,4 @@
 // massive thanks to https://github.com/antimatter15/rgb-lab for providing a good example of an implementation
-
-use num::integer::Roots;
-
 use super::rgb::RgbPixel;
 
 #[derive(Debug, Clone, Copy)]
@@ -16,7 +13,51 @@ impl From<(f32, f32, f32)> for LabPixel {
 
 impl From<RgbPixel> for LabPixel {
     fn from(value: RgbPixel) -> Self {
-        let (r, g, b) = value.get();
+        Self::from_rgb(&value)
+    }
+}
+
+impl Into<RgbPixel> for LabPixel {
+    fn into(self) -> RgbPixel {
+        self.as_rgb()
+    }
+}
+
+impl LabPixel {
+    pub fn get(&self) -> (f32, f32, f32) {
+        (self.0, self.1, self.2)
+    }
+
+    pub fn distance_from(&self, other: &LabPixel) -> f32 {
+        let delta_l = self.0 - other.0;
+        let delta_a = self.1 - other.1;
+        let delta_b = self.2 - other.2;
+
+        let c1 = self.1.powf(2.0) + self.2.powf(2.0);
+        let c2 = other.1.powf(2.0) + other.2.powf(2.0);
+
+        let delta_c = c1 - c2;
+        let delta_h = delta_a.powf(2.0) + delta_b.powf(2.0) + delta_c.powf(2.0);
+        let delta_h = if delta_h < 0.0 { 0.0 } else {delta_h.sqrt()};
+
+        let sc = 1.0 + 0.045 * c1;
+        let sh = 1.0 + 0.015 * c1;
+
+        let delta_l_kl_sl = delta_l / (1.0);
+        let delta_c_kc_sc = delta_c / sc;
+        let delta_h_kh_sh = delta_h / sh;
+
+        let i = delta_l_kl_sl.powf(2.0) + delta_c_kc_sc.powf(2.0) + delta_h_kh_sh.powf(2.0);
+        
+        if i < 0.0 {
+            0.0
+        } else {
+            i.sqrt()
+        }
+    }
+
+    pub fn from_rgb(rgb: &RgbPixel) -> LabPixel {
+        let (r, g, b) = rgb.get();
         let (mut r, mut g, mut b) = (
             r as f32 / 255.0,
             b as f32 / 255.0,
@@ -55,10 +96,8 @@ impl From<RgbPixel> for LabPixel {
             200.0 * (y - z),
         )
     }
-}
 
-impl Into<RgbPixel> for LabPixel {
-    fn into(self) -> RgbPixel {
+    pub fn as_rgb(&self) -> RgbPixel {
         let (l, a, b) = self.get();
 
         let mut y = (l + 16.0) / 116.0;
@@ -96,40 +135,6 @@ impl Into<RgbPixel> for LabPixel {
             (g.clamp(0.0, 1.0) * 255.0) as u8,
             (b.clamp(0.0, 1.0) * 255.0) as u8,
         )
-    }
-}
-
-impl LabPixel {
-    pub fn get(&self) -> (f32, f32, f32) {
-        (self.0, self.1, self.2)
-    }
-
-    pub fn distance_from(&self, other: &LabPixel) -> f32 {
-        let delta_l = self.0 - other.0;
-        let delta_a = self.1 - other.1;
-        let delta_b = self.2 - other.2;
-
-        let c1 = self.1.powf(2.0) + self.2.powf(2.0);
-        let c2 = other.1.powf(2.0) + other.2.powf(2.0);
-
-        let delta_c = c1 - c2;
-        let delta_h = delta_a.powf(2.0) + delta_b.powf(2.0) + delta_c.powf(2.0);
-        let delta_h = if delta_h < 0.0 { 0.0 } else {delta_h.sqrt()};
-
-        let sc = 1.0 + 0.045 * c1;
-        let sh = 1.0 + 0.015 * c1;
-
-        let delta_l_kl_sl = delta_l / (1.0);
-        let delta_c_kc_sc = delta_c / sc;
-        let delta_h_kh_sh = delta_h / sh;
-
-        let i = delta_l_kl_sl.powf(2.0) + delta_c_kc_sc.powf(2.0) + delta_h_kh_sh.powf(2.0);
-        
-        if i < 0.0 {
-            0.0
-        } else {
-            i.sqrt()
-        }
     }
 }
 
