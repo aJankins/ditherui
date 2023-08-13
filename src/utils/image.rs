6c4120f9e16ba4dc2d22d29ddaec1b76429e0efc
ajfyb <mctech26@gmail.com>
2023-08-13 16:31:13 +0200

    Added gif encoder

--- src/utils/image.rs
+++ src/utils/image.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
-use std::io::Cursor;
+use std::fs::File;
+use std::io::{Cursor, Read};
 
 use base64::Engine;
+use image::codecs::gif::GifDecoder;
 use image::io::Reader as ImageReader;
-use image::{self, imageops, DynamicImage, GenericImageView};
+use image::{self, imageops, DynamicImage, GenericImageView, Frame, AnimationDecoder};
 
 type UtilResult<T> = Result<T,Box<dyn Error>>;
 
@@ -41,6 +43,32 @@ impl<'a> ImageRequest<'a> {
     }
 }
 
+pub enum GifRequest<'a> {
+    Url {
+        url: &'a str,
+        // max_dim: Option<usize>,
+    },
+    File{
+        file: &'a str,
+        // max_dim: Option<usize>,
+    },
+}
+
+impl<'a> GifRequest<'a> {
+    pub fn perform(&self) -> UtilResult<DynamicImage> {
+        match self {
+            GifRequest::File {
+                file,
+                // max_dim 
+            } => load_image_from_path(file),
+            GifRequest::Url {
+                url,
+                // max_dim
+            } => load_image_from_url(url),
+        }
+    }
+}
+
 pub fn image_to_b64(image: &DynamicImage) -> UtilResult<String> {
     let mut buf = Vec::new();
     image.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Png)?;
@@ -71,6 +99,8 @@ fn resize_image_with_max_dim(image: &DynamicImage, maxdim: u32) -> DynamicImage 
 
 // Loader functions
 
+// image
+
 fn load_image_from_path(path: &str) -> UtilResult<DynamicImage> {
     Ok(ImageReader::open(path)?.decode()?)
 }
@@ -88,4 +118,20 @@ fn load_image_from_url(url: &str) -> UtilResult<DynamicImage> {
 fn load_image_from_url_with_max_dim(url: &str, maxdim: u32) -> UtilResult<DynamicImage> {
     let image = load_image_from_url(url)?;
     Ok(resize_image_with_max_dim(&image, maxdim))
+}
+
+// gif
+
+fn load_gif_from_file(path: &str) -> UtilResult<Vec<Frame>> {
+    let file = File::open(path).unwrap();
+    Ok(GifDecoder::new(file)?.into_frames().collect_frames()?)
+}
+
+fn load_gif_from_url(url: &str) -> UtilResult<Vec<Frame>> {
+    let mut gif_bytes = reqwest::blocking::get(url)?;
+    
+    let mut data = Vec::new();
+    gif_bytes.read_to_end(&mut data)?;
+    
+    Ok(GifDecoder::new(data.as_slice())?.into_frames().collect_frames()?)
 }
